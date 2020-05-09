86169f8d27a9cc6145bb557a6126f5ad1bc17205
DhiraPT111 <dhira.pt@gmail.com>
2020-05-09 17:40:13 +0800

    1st

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+'''@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))'''
+
+if __name__ == "__main__":
+    app.run(debug=True)
